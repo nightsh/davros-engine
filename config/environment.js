eb69e76905d32eba78fd40879523535e7e1083fe
Victor Nițu <victor@thesponge.eu>
2017-06-20 13:47:51 +0300

    Basic engine preparations

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,11 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment/* , appConfig */) {
+  const ENV = {
+    modulePrefix: 'davros-engine',
+    environment: environment
+  };
+
+  return ENV;
 };
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-env node */
+const EngineAddon = require('ember-engines/lib/engine-addon');
 'use strict';
 
-module.exports = {
-  name: 'davros-engine'
+module.exports = EngineAddon.extend({
+  name: 'davros-engine',
+  lazyLoading: false
 };
